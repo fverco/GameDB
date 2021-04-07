2fb7c5143f3160d331d73e9c5a635c70b97ea588
Fieryfox <fieryfox@fieryfox.com>
2021-04-07 23:01:21 +0200

    Add the first comments of the project in main.cpp

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,7 +1,23 @@
+/*! \mainpage Welcome
+ *
+ *  \section build-info Build Information
+ *
+ *  This application uses Qt 6.0.3 and uses Qt Quick.\n
+ *  Compiled with MinGW 64-bit and uses SQLite as its database.\n
+ *  Documentation is done with a combination of Doxygen and Graphviz.\n
+ *
+ */
+
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 
+/*!
+ * \brief Start the application.
+ * \param argc The amount of arguments passed to the application at start.
+ * \param argv A list of all arguments passed to the application at start.
+ * \return An integer that shows whether the application was successfull or failed.
+ */
 int main(int argc, char *argv[])
 {
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
