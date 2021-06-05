9562e37542d271e3f83b80f783417e07ff99155b
Fieryfox <fieryfox@fieryfox.com>
2021-06-05 12:45:32 +0200

    Revamp ImageDatabase to change the way images are stored
    The ImageDatabase is revamped to now store all the IDs of all the covers
    and their file suffixes in the SQLite database.

--- headers/imagedatabase.h
+++ headers/imagedatabase.h
@@ -9,15 +9,6 @@
 #include <QSqlDatabase>
 
 /*!
- * \brief Enum for all the supported image formats by the application.
- */
-enum class ImageFormats {
-    JPG,    ///< 0
-    PNG,    ///< 1
-    BMP     ///< 2
-};
-
-/*!
  * \brief The class for images in the database.
  * \note This class stores its images in the file system, but stores information about the images in an SQLite database.
  */
@@ -25,31 +16,38 @@ class ImageDatabase : public Database
 {
     Q_OBJECT
 public:
-    explicit ImageDatabase(QObject *parent = nullptr);
+    explicit ImageDatabase(const QString &conName, QObject *parent = nullptr);
     bool createDatabase(const QString &imgDir = QGuiApplication::applicationDirPath() + "/images");
 
     bool addCover(const QString &filePath);
-    bool addCover(const QByteArray &img, const ImageFormats &imgFormat);
-    bool addCover(const Game &game, const ImageFormats &imgFormat);
+    bool addCover(const QPixmap &img, const QString &suffix);
+    bool addCover(const QByteArray &img, const QString &suffix);
+    bool addCover(const Game &game, const QString &suffix);
 
     int getLastCoverId() const;
+    QString getCoverPath(const int &coverId);
+    QString getCoverSuffix(const int &coverId);
 
     static QString getFileSuffix(const QString &filePath);
+    static QString removeFileSuffix(QString fileName);
+
+    const static QList<QString> supportedSuffixes;  ///< List of supported image file suffixes. The entire ImageDatabase gets its suffix information from this list.
 
 signals:
+    void imageDatabaseReset();  ///< Emitted when the CoverDB is cleared. For informing other classes that the old cover IDs no longer exist.
 
 public slots:
 
 private:
     QSqlDatabase coverDataDb;   ///< Database used to store information about the covers.
-    QString coverDataLocation;  ///< The file path to the coverDataDb
-    int lastCoverId;            ///< ID of the last cover image to be added to the DB
+    QString coverDataLocation;  ///< The file path to the coverDataDB.
+    int lastCoverId;            ///< The ID of the last cover in the database.
 
     void openDb();
     void closeDb();
     bool createCoverDataDB();
-    bool setLastCoverDB(const int &id);
-    bool getLastCoverDB();
+    bool readImagesToCoverDataDB();
+    int addCoverData(const QString &fileSuffix);
 };
 
 #endif // IMAGEDATABASE_H
--- sources/imagedatabase.cpp
+++ sources/imagedatabase.cpp
@@ -6,13 +6,17 @@
 #include <QSqlQuery>
 #include <QSqlError>
 
+const QList<QString> ImageDatabase::supportedSuffixes({"jpg", "jpeg", "png", "bmp"});
+
 /*!
  * \brief Instantiates the image database object.
- * \param parent = The QObject to which this database is binded.
+ * \param conName = The name of the connection (Must be unique)
+ * \param parent = The QObject to which this database is binded
+ * \note By default it specifies the CoverDataDB as a file named "image.data" in the application root, but doesn't create it yet.
  */
-ImageDatabase::ImageDatabase(QObject *parent) :
+ImageDatabase::ImageDatabase(const QString &conName, QObject *parent) :
     Database(parent),
-    coverDataDb(QSqlDatabase::addDatabase("QSQLITE", "imageId")),
+    coverDataDb(QSqlDatabase::addDatabase("QSQLITE", conName)),
     coverDataLocation(QGuiApplication::applicationDirPath() + "/image.data"),
     lastCoverId(-1)
 {
@@ -20,128 +24,159 @@ ImageDatabase::ImageDatabase(QObject *parent) :
 
 /*!
  * \brief Creates the image database if it doesn't exist yet.
- * \param imgDir = The full path to where the database should be stored.
+ * \param imgDir = The full path to where the images should be stored.
  * \return A boolean value that states whether the creation was successful or not.
- * \note This creates the images directory and the image.data database file if they don't already exist.
+ * \note This creates the images directory and the CoverDataDB, specified by the constructor, if they don't already exist.
+ * If the images directory doesn't exist then it will create it, reset the CoverDataDB and emit the signal imageDatabaseReset().
+ * If only the CoverDataDB file is not found, then it will create it and detect all the images already in the images directory.
  */
 bool ImageDatabase::createDatabase(const QString &imgDir) {
     location = imgDir;
 
     if (!QDir(location).exists()) {
         if (QDir().mkdir(location)) {
-            if (!createCoverDataDB())
+            if (QFile::remove(coverDataLocation)) {
+                if (createCoverDataDB())
+                    emit imageDatabaseReset();
+                else
+                    return false;
+            } else
                 return false;
         } else
             return false;
     }
 
     if (!QFile::exists(coverDataLocation)) {
-        if (!createCoverDataDB())
+        if (createCoverDataDB())
+            return readImagesToCoverDataDB();
+        else
             return false;
-
-        QDir directory(location);
-        QStringList images(directory.entryList(QStringList() << "*.jpg"
-                                                            << "*.JPG"
-                                                            << "*.jpeg"
-                                                            << "*.JPEG"
-                                                            << "*.png"
-                                                            << "*.PNG"
-                                                            << "*.bmp"
-                                                            << "*.BMP" ,QDir::Files, QDir::Name));
-        QString lastImage(images.at(images.length()-1));
-        QString lastImageName("");
-
-        for (int i(0); i < lastImage.length(); ++i) {
-            if (lastImage.at(i) != '.') {
-                lastImageName += lastImage.at(i);
-            } else
-                break;
-        }
-
-        lastCoverId = lastImageName.toInt();
-        setLastCoverDB(lastImageName.toInt());
     }
 
-    return getLastCoverDB();
+    return true;
 }
 
 /*!
  * \brief Adds a new cover image to the database by loading it from a given file path.
  * \param filePath = The path to the image file
  * \return A boolean value that states whether the operation was successful or not.
+ * \note This is an overloaded function.
  * \note To obtain the new cover's ID, call the getLastCoverId() function.
  * \see getLastCoverId()
  */
 bool ImageDatabase::addCover(const QString &filePath) {
     QPixmap pixImg(filePath);
-    QString format(getFileSuffix(filePath));
+    QString suffix(getFileSuffix(filePath));
+
+    return addCover(pixImg, suffix);
+}
+
+/*!
+ * \brief Adds a new cover image to the database by loading it from a QPixmap.
+ * \param img = The QPixmap image
+ * \param suffix = The file suffix
+ * \return A boolean value that states whether the operation was successful or not.
+ * \note To obtain the new cover's ID, call the getLastCoverId() function.
+ * \see getLastCoverId()
+ */
+bool ImageDatabase::addCover(const QPixmap &img, const QString &suffix)
+{
     bool success(false);
+    int coverId(addCoverData(suffix));
 
-    success = pixImg.save(getDBLocation() + "/" + QString::number(++lastCoverId) + "." + format, format.toStdString().c_str());
-
-    if (success) {
-        setLastCoverDB(lastCoverId);
-        return true;
+    if (coverId > -1) {
+        success = img.save(getDBLocation() + "/" + QString::number(coverId) + "." + suffix.toLower(), suffix.toStdString().c_str());
+        lastCoverId = coverId;
+        return success;
     }
 
     return false;
 }
 
 /*!
- * \brief Adds a new cover image to the database.
- * \param img = The image
- * \param imgFormat = The file format of the image
+ * \brief Adds a new cover image to the database by loading it from a QByteArray.
+ * \param img = The QByteArray image
+ * \param suffix = The file suffix
  * \return A boolean value that states whether the operation was successful or not.
+ * \note This is an overloaded function.
  * \note To obtain the new cover's ID, call the getLastCoverId() function.
  * \see getLastCoverId()
  */
-bool ImageDatabase::addCover(const QByteArray &img, const ImageFormats &imgFormat) {
+bool ImageDatabase::addCover(const QByteArray &img, const QString &suffix) {
     QPixmap pixImg;
     pixImg.loadFromData(img);
-    bool success(false);
 
-    const char* format("");
-
-    switch (imgFormat) {
-        case ImageFormats::JPG: format = "jpg";
-                                break;
-        case ImageFormats::PNG: format = "png";
-                                break;
-        case ImageFormats::BMP: format = "bmp";
-                                break;
-        default: format = "jpg";
-    }
-
-    success = pixImg.save(getDBLocation() + "/" + QString::number(++lastCoverId), format);
-
-    if (success) {
-        setLastCoverDB(lastCoverId);
-        return true;
-    }
-
-    return false;
+    return addCover(pixImg, suffix);
 }
 
 /*!
- * \brief Adds a new cover image to the database.
+ * \brief Adds a new cover image to the database by loading the QByteArray from a Game object.
  * \param game = A game object with a cover image
- * \param imgFormat = The file format of the image
+ * \param suffix = The file suffix
  * \return A boolean value that states whether the operation was successful or not.
- * \note This function is exactly the same as addCover(const QByteArray &img, const ImageFormats &imgFormat).
+ * \note This is an overloaded function.
  * \note To obtain the new cover's ID, call the getLastCoverId() function.
  * \see getLastCoverId()
  */
-bool ImageDatabase::addCover(const Game &game, const ImageFormats &imgFormat) {
-    return addCover(game.getCoverImage(), imgFormat);
+bool ImageDatabase::addCover(const Game &game, const QString &suffix) {
+    return addCover(game.getCoverImage(), suffix);
 }
 
 /*!
- * \brief Returns the ID of the last cover to be added to the DB.
- * \return An integer with the ID of the last added cover.
- * \note This is not the same as getLastCoverDB(). Unlike getLastCoverDB() this call does not make any database queries.
+ * \brief Retrieves the ID of the last cover image added to the database.
+ * \return An Integer with the cover ID. Returns -1 if no cover image has ever been added.
  */
 int ImageDatabase::getLastCoverId() const {
     return lastCoverId;
+}
+
+/*!
+ * \brief Retrieves the full file path to a cover image.
+ * \param coverId = The ID of the cover image in the database
+ * \return A QString with the full file path. Returns an empty QString if that cover isn't found or an error has occurred.
+ * \note This function uses SQL queries.
+ */
+QString ImageDatabase::getCoverPath(const int &coverId)
+{
+    QString coverPath("");
+    QString coverSuffix(getCoverSuffix(coverId));
+
+    coverPath = location + "/" + QString::number(coverId) + "." + coverSuffix;
+
+    if(!QFile::exists(coverPath))
+        coverPath = "";
+
+    return coverPath;
+}
+
+/*!
+ * \brief Retrieves the suffix/image format of the cover in the database.
+ * \param coverId = The ID of the cover image in the database
+ * \return QString with the suffix/image format. Returns an empty QString if that cover isn't found or an error has occurred.
+ */
+QString ImageDatabase::getCoverSuffix(const int &coverId)
+{
+    openDb();
+
+    QString coverSuffix("");
+    QSqlQuery imageQry(coverDataDb);
+
+    imageQry.prepare("SELECT suffix "
+                     "FROM CoverData "
+                     "WHERE cover_id = ?;");
+    imageQry.addBindValue(coverId);
+
+    if (imageQry.exec()) {
+        if (imageQry.next()) {
+            coverSuffix = imageQry.value(0).toString();
+
+            closeDb();
+            return coverSuffix;
+        }
+    }
+
+    closeDb();
+    return coverSuffix;
 }
 
 /*!
@@ -163,7 +198,27 @@ QString ImageDatabase::getFileSuffix(const QString &filePath) {
 }
 
 /*!
- * \brief Opens a connection to the database file, if it's not open already.
+ * \brief Returns a file name without the file suffix.
+ * \param fileName = The full path to the file or just the file name.
+ * \return QString of the file name with the file suffix removed.
+ */
+QString ImageDatabase::removeFileSuffix(QString fileName)
+{
+    // Move nameSize to the last letter in fileName.
+    int nameSize(fileName.length() - 1);
+
+    // Count down until you reach the dot seperator.
+    for (; fileName.at(nameSize) != '.'; --nameSize) {}
+
+    // Find the suffix size by subtracting fileName size with the name size,
+    // then remove that amount of letters from the end.
+    fileName.chop(fileName.size() - nameSize);
+
+    return fileName;
+}
+
+/*!
+ * \brief Opens a connection to the CoverData database file, if it's not open already.
  */
 void ImageDatabase::openDb() {
     if (!coverDataDb.isOpen()) {
@@ -173,7 +228,7 @@ void ImageDatabase::openDb() {
 }
 
 /*!
- * \brief Closes the connection to the database file, if it is open.
+ * \brief Closes the connection to the CoverData database file, if it is open.
  */
 void ImageDatabase::closeDb() {
     if (coverDataDb.isOpen())
@@ -191,43 +246,70 @@ bool ImageDatabase::createCoverDataDB() {
         QSqlQuery imageQry(coverDataDb);
 
         imageQry.prepare("CREATE TABLE CoverData ("
-                         "last_cover_id INTEGER UNIQUE NOT NULL PRIMARY KEY);");
+                         "cover_id INTEGER UNIQUE NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                         "suffix TEXT NOT NULL);");
 
         if (!imageQry.exec()) {
-            closeDb();
-            return false;
-        }
-
-        imageQry.prepare("INSERT INTO CoverData (last_cover_id) "
-                         "VALUES (-1);");
-
-        if (!imageQry.exec()) {
+            qDebug() << "(ImageDB) Cover data DB creation error: " << imageQry.lastError().text() << Qt::endl;
             closeDb();
             return false;
         }
 
         closeDb();
-    }
+    } else
+        return false;
 
     return true;
 }
 
 /*!
- * \brief Updates the last cover ID in the database to the provided ID.
- * \param id = The new last cover ID
+ * \brief Detects all the image files in the images directory and records their data to the CoverData database.
  * \return A boolean value that states whether the operation was successful or not.
- * \note This function must be called every time the lastCoverId is changed to ensure it persists each time the application is closed.
+ * \note This ONLY detects image files correctly named from the number 1 and up.
  */
-bool ImageDatabase::setLastCoverDB(const int &id) {
+bool ImageDatabase::readImagesToCoverDataDB()
+{
+    QDir directory(location);
+
+    // Create a filter list of all supported image files.
+    QStringList fileFilterList;
+    for (int i(0); i < supportedSuffixes.size(); ++i) {
+        fileFilterList << "*." + supportedSuffixes.at(i);
+        fileFilterList << "*." + supportedSuffixes.at(i).toUpper();
+    }
+
+    // Get a list of all image files in the existing images directory.
+    QStringList images(directory.entryList(fileFilterList, QDir::Files, QDir::Name));
+
+    int fileNameId(0);
+    QString fileSuffix("");
+    QVariantList idList,  suffixList;   // Lists of all recorded files to be added in bulk to the database.
+
+    // Obtain the names and suffixes of all image files and add them to the lists.
+    // NOTE: This will only record the files correctly named from 1 and up.
+    for (int i(0); i < images.size(); ++i) {
+
+        fileNameId = removeFileSuffix(images.at(i)).toInt();
+        fileSuffix = getFileSuffix(images.at(i));
+
+        if (fileNameId == i + 1) {
+            idList << fileNameId;
+            suffixList << fileSuffix;
+        }
+    }
+
     openDb();
 
     QSqlQuery imageQry(coverDataDb);
 
-    imageQry.clear();
-    imageQry.prepare("UPDATE CoverData SET last_cover_id = :id");
-    imageQry.bindValue(":id", id);
+    imageQry.prepare("INSERT INTO CoverData (cover_id, suffix) "
+                     "VALUES (?, ?);");
 
-    if (!imageQry.exec()) {
+    imageQry.addBindValue(idList);
+    imageQry.addBindValue(suffixList);
+
+    if (!imageQry.execBatch()) {
+        qDebug() << "(ImageDB) Cover data DB error detecting images: " << imageQry.lastError().text() << Qt::endl;
         closeDb();
         return false;
     }
@@ -237,29 +319,30 @@ bool ImageDatabase::setLastCoverDB(const int &id) {
 }
 
 /*!
- * \brief Retrieves the last cover ID stored in the database and applies it to lastCoverId.
- * \return A boolean value that states whether the operation was successful or not.
- * \note This function is not the same as getLastCoverId(). This function must ONLY be called if absolutely necessary, such as during boot up of the application.\n If you simply need to get the last cover ID, call getLastCoverId() instead.
- * \see getLastCoverId()
+ * \brief Creates a new cover ID in the database and adds the given suffix to it.
+ * \param fileSuffix = The file suffix of the new cover image
+ * \return An integer with the new cover ID. Returns -1 if an error occurs.
  */
-bool ImageDatabase::getLastCoverDB() {
-    openDb();
+int ImageDatabase::addCoverData(const QString &fileSuffix)
+{
+    if (supportedSuffixes.contains(fileSuffix.toLower())) {
 
-    QSqlQuery imageQry(coverDataDb);
+        openDb();
 
-    imageQry.prepare("SELECT last_cover_id FROM CoverData");
-    if (imageQry.exec()) {
-        if(imageQry.next())
-            lastCoverId = imageQry.value(0).toInt();
-        else {
+        QSqlQuery imageQry(coverDataDb);
+
+        imageQry.prepare("INSERT INTO CoverData (suffix) "
+                         "VALUES (?);");
+        imageQry.addBindValue(fileSuffix);
+
+        if (imageQry.exec()) {
+            int lastId(imageQry.lastInsertId().toInt());
             closeDb();
-            return false;
+            return lastId;
         }
-    } else {
+
         closeDb();
-        return false;
     }
 
-    closeDb();
-    return true;
+    return -1;
 }
